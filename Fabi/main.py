6d903c6ecb7f569a2479ba74ede381f6a4add7de
PeZi98 <92920662+PeZi98@users.noreply.github.com>
2021-10-21 18:15:25 +0200

    Update main.py

--- Fabi/main.py
+++ Fabi/main.py
@@ -1,5 +1,9 @@
 
 import pandas as pd
+import numpy as np
+
+df1 = pd.read_csv('Longitude.csv').to_numpy()
+df2 = pd.read_csv('Latitude.csv').to_numpy()
 
 if __name__ == '__main__':
     df = pd.read_csv("master_dataset.csv", sep=',')
